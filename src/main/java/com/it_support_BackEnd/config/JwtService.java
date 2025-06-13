9ab1d878ba32f54f478cbe70750cb9ce100fea20
faTimaezzahrarguiti <fatimazahraraguti@gmail.com>
2025-06-13 16:33:07 +0100

    Security update

--- src/main/java/com/it_support_BackEnd/config/JwtService.java
+++ src/main/java/com/it_support_BackEnd/config/JwtService.java
@@ -23,7 +23,7 @@ public class JwtService {
         return extractClaim(token, Claims::getSubject);
     }
 
-    public <T> T extractClaim(String tokens, Function<Claims,T> claimsResolver) {
+    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
